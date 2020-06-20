ef10b48f2af89eca2e7b2a726fec62ececfd3394
mitesharora <mitesharora96@gmail.com>
2020-06-20 10:13:46 +0530

    Transfered control from admid layout to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-
-
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
-
 import { AppComponent } from './app.component';
-
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserProfileComponent } from './my-account/user-profile.component';
 import { TableListComponent } from './my-wallet/table-list.component';
+import { TypographyComponent } from './transactions/typography.component';
 import { IconsComponent } from './Report/icons.component';
 import { MapsComponent } from './maps/maps.component';
 import { NotificationsComponent } from './notifications/notifications.component';
@@ -23,6 +20,7 @@ import {
 } from '@agm/core';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { DbService } from './db.service';
+import {MaterialModule} from './material.module'
 
 
 @NgModule({
@@ -34,13 +32,18 @@ import { DbService } from './db.service';
     ComponentsModule,
     RouterModule,
     AppRoutingModule,
+    MaterialModule,
     AgmCoreModule.forRoot({
       apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
     })
   ],
   declarations: [
     AppComponent,
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    UserProfileComponent,
+    TableListComponent,
+    TypographyComponent,
+    IconsComponent
   ],
 
   //entryComponents: [
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatRippleModule} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSelectModule} from '@angular/material/select';
 import {MatTableModule} from '@angular/material/table';
 
 
 const MaterialsComponent=[
     MatButtonModule,
-    MatTableModule
+    MatTableModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatTooltipModule,
+    MatSelectModule,
+    MatTableModule,
+    MatInputModule
 ]
 
 
